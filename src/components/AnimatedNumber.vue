f68348bcef9270aaa97d3faaf80ad2a7b193c795
Naresh <naresh.khatri2345@gmail.com>
2022-03-12 19:06:46 +0530

    create AnimatedNumber component to twee numbers

---
+++ src/components/AnimatedNumber.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    {{ animatedNumber }}
+  </div>
+</template>
+<script setup>
+import { ref, computed, onMounted } from "vue";
+
+const props = defineProps(["number"]);
+const animatedNumber = ref(props.number);
+onMounted(() => { 
+  randomlyIncreaseNumber();
+  console.log(props.number);
+});
+const randomlyIncreaseNumber = async () => {
+  console.log("okay");
+  let updateTime = 1;
+  while (true) {
+    await sleep(3000);
+    console.log("next update in: ", updateTime);
+    animatedNumber.value++;
+    updateTime = Math.random() * 7000;
+  }
+};
+function sleep(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+</script>
