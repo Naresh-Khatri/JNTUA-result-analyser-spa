4d7006a1c64050fc2cbb89967c375ccc9c986848
Naresh <naresh.khatri2345@gmail.com>
2022-03-12 19:04:47 +0530

    Update stats.vue to composition api

--- src/pages/stats.vue
+++ src/pages/stats.vue
@@ -1,14 +1,17 @@
 <template>
-  <q-page padding class="container flex flex-center" :key="pieDataCollection.labels.length">
+  <q-page padding class="container flex flex-center">
+    <!-- :key="pieDataCollection.labels.length" -->
     <div class="leaderboards">
       <div class="flex flex-center">
         <div
           class="q-my-md q-py-md row rounded search-count-container"
           :class="$q.dark.isActive ? 'bg-dark' : 'bg-white'"
-          style="width: 60%;"
+          style="width: 60%"
         >
           <div class="col-8">
-            <div class="text-h4 text-center">{{ animatedNumber }}</div>
+            <div class="text-h4 text-center">
+              <AnimatedNumber v-if="totalSearches" :number="totalSearches" />
+            </div>
           </div>
           <div class="col-4">
             <span class="row text-caption">total</span>
@@ -23,19 +26,19 @@
       >
         <div class="text-h4 q-mt-lg q-ml-lg">🏆Leaderboards</div>
         <div class="text-body1 q-ma-md">
-          <i class="fas fa-crown" style="color:gold"></i>
+          <i class="fas fa-crown" style="color: gold"></i>
           {{ topColleges[0].collegeCode }} - {{ topColleges[0].collegeName }}({{
             topColleges[0].district
           }})
         </div>
         <div class="text-body1 q-ma-md">
-          <i class="fas fa-crown" style="color:silver"></i>
+          <i class="fas fa-crown" style="color: silver"></i>
           {{ topColleges[1].collegeCode }} - {{ topColleges[1].collegeName }}({{
             topColleges[1].district
           }})
         </div>
         <div class="text-body1 q-ma-md">
-          <i class="fas fa-crown" style="color:brown"></i>
+          <i class="fas fa-crown" style="color: brown"></i>
           {{ topColleges[2].collegeCode }} - {{ topColleges[2].collegeName }}({{
             topColleges[2].district
           }})
@@ -47,185 +50,188 @@
       class="q-my-md q-py-md rounded pie-container"
       :class="$q.dark.isActive ? 'bg-dark' : 'bg-white'"
     >
-      <div class="text-center text-h6 q-ma-md">College code (Searches count)</div>
-      <PieChart style="height:600px" :chart-data="pieDataCollection" :key="$q.dark.isActive" />
-      <div class="q-pa-md text-right text-grey-6">*Note: Only top 30 colleges out of {{collegesReached}} are shown</div>
+      <div class="text-center text-h6 q-ma-md">
+        College code (Searches count)
+      </div>
+      <VueApexCharts
+        width="100%"
+        height="350"
+        type="donut"
+        :options="totalSearchesOptions"
+        :series="totalSearchesSeries"
+      />
+      <div class="q-pa-md text-right text-grey-6">
+        *Note: Only top 30 colleges out of {{ collegesReached }} are shown
+      </div>
     </div>
     <div
       class="flex column rounded q-pa-md q-my-md searches-container"
       :class="$q.dark.isActive ? 'bg-dark' : 'bg-white'"
-      style="border-radius:20px"
+      style="border-radius: 20px"
     >
       <div class="text-h5">searches per day</div>
       <q-scroll-area
         ref="scrollArea"
         horizontal
-        style="height:400px;"
+        style="height: 400px"
         :thumb-style="{
           bottom: '4px',
           borderRadius: '5px',
           background: '#b0b8b4ff',
           width: '10px',
-          opacity: 1
+          opacity: 1,
         }"
         :style="!$q.screen.lt.md ? 'height:400px' : ''"
       >
-        <LineChart
-          :style="`width:${20 * lineDataCollection.labels.length}px`"
-          :chart-data="lineDataCollection"
-          :key="$q.dark.isActive"
+        <VueApexCharts
+          width="100%"
+          height="350"
+          type="line"
+          :options="dailySearchesOptions"
+          :series="dailySearchesSeries"
         />
       </q-scroll-area>
     </div>
   </q-page>
 </template>
 
-<script>
-// import data from './data.json'
+<script setup>
+import { ref, computed, onMounted } from "vue";
+import { useQuasar } from "quasar";
+import VueApexCharts from "vue3-apexcharts";
+import AnimatedNumber from "../components/AnimatedNumber.vue";
 import gsap from "gsap";
-import PieChart from "../charts/PieChart.vue";
-import LineChart from "../charts/LineChart.vue";
 import axios from "axios";
 
 import apiRoutes from "../apiRoutes";
-
 import { backgroundColors, borderColors } from "../colors/colors";
 
-export default {
-  // name: 'PageName',
-  components: {
-    PieChart,
-    LineChart
-  },
-  data() {
-    return {
-      backgroundColors: backgroundColors,
-      borderColors: borderColors,
-      pieDataCollection: {
-        labels: [],
-        datasets: [
-          {
-            data: [],
-            backgroundColor: [],
-            borderColor: [],
-            borderWidth: 1
-          }
-        ]
-      },
-      lineDataCollection: {
-        labels: [],
-        datasets: [
-          {
-            label: "Search Count",
-            data: [],
-            backgroundColor: [],
-            borderColor: [],
-            borderWidth: 1
-          }
-        ]
-      },
-      topColleges: [],
-      collegesReached: 0,
-      totalSearches: 0,
-      tweenedNumber: 0,
-      timeout: null,
-      updateTime: 1000
-    };
-  },
+const topColleges = ref([]);
+const collegesReached = ref(0);
+const totalSearches = ref(0);
+const tweenedNumber = ref(0);
+const timeout = ref(null);
+const updateTime = ref(1000);
+const scrollArea = ref(null);
+const dateChart = ref(null);
+const totalSearchesOptions = ref({});
+const totalSearchesSeries = ref();
+const dailySearchesOptions = ref({
+  chart: { id: "vuechart-example" },
+  theme: { palette: "palette1" },
+});
+const dailySearchesSeries = ref([]);
+// const lineDataCollection = ref({});
+// const pieDataCollection = ref({});
 
-  computed: {
-    animatedNumber() {
-      return this.tweenedNumber.toFixed(0);
-    }
-  },
-  watch: {
-    totalSearches(val) {
-      gsap.to(this.$data, { duration: 1, tweenedNumber: val });
-    }
-  },
-  mounted() {
-    // this.pieDataCollection.data = data.
-    // console.log(data)
-    this.updateSearchCount();
-    setTimeout(() => {
-      console.log(this.pieDataCollection);
-    }, 1000);
-    axios
-      .get(apiRoutes.stats)
-      .then(res => {
-        console.log(res);
-        // setTimeout(() => {
-        //   this.$refs.scrollArea.setScrollPosition(1000, 1000);
-        // }, 500);
-        this.topColleges = res.data.topColleges;
-        this.totalSearches = res.data.totalSearches;
-        this.collegesReached = res.data.searches.length
+const animatedNumber = computed(() => tweenedNumber.value.toFixed(0));
+// watch: {
+//   totalSearches(val) {
+//     gsap.to(this.$data, { duration: 1, tweenedNumber: val });
+//   }
+// };
+onMounted(() => {
+  // updateSearchCount();
+  setTimeout(() => {
+    // console.log(pieDataCollection.value);
+  }, 1000);
+  axios
+    .get(apiRoutes.stats)
+    .then((res) => {
+      console.log(res);
+      // setTimeout(() => {
+      //   this.$refs.scrollArea.setScrollPosition(1000, 1000);
+      // }, 500);
+      topColleges.value = res.data.topColleges;
+      totalSearches.value = res.data.totalSearches;
+      collegesReached.value = res.data.searches.length;
+      // gsap.to(tweenedNumber.value, { duration: 1, tweenedNumber: totalSearches.value });
 
-        let counter = 0;
-        // let arr = Object.entries(res.data.colleges);
-        // arr.sort(([a, b], [c, d]) => d - b);
-        // console.log(arr);
-        res.data.searches.map((item, index) => {
-          if (index >= 30) return;
-          this.pieDataCollection.labels.push(
-            `${item["_id"]} (${item["total"]})`
-          );
-          this.pieDataCollection.datasets[0].data.push(item["total"]);
-          this.pieDataCollection.datasets[0].backgroundColor.push(
-            backgroundColors[counter % 7]
-          );
-          this.pieDataCollection.datasets[0].borderColor.push(
-            borderColors[counter % 7]
-          );
-          counter++;
-        });
-        // // arr = Object.entries(res.data.searchDates);
-        //set the line chart color to primary
-        this.lineDataCollection.datasets[0].backgroundColor.push(
-          backgroundColors[1]
-        );
-        this.lineDataCollection.datasets[0].borderColor.push(borderColors[1]);
+      let counter = 0;
+      // let arr = Object.entries(res.data.colleges);
+      // arr.sort(([a, b], [c, d]) => d - b);
+      // console.log(arr);
+      const top30CollegeNames = [];
+      const top30CollegeSearchCounts = [];
+      res.data.searches.map((item, index) => {
+        if (index >= 30) return;
+        // pieDataCollection.value.labels.push(
+        //   `${item["_id"]} (${item["total"]})`
+        // );
+        // console.log("pushing ", item["total"], item["_id"]);
+        top30CollegeNames.push(`${item["_id"]} (${item["total"]})`);
+        top30CollegeSearchCounts.push(item["total"]);
+        // pieDataCollection.value.datasets[0].data.push(item["total"]);
 
-        res.data.searchesArr.map((item, index) => {
-          this.lineDataCollection.labels.push(item["date"].slice(5, 10));
-          this.lineDataCollection.datasets[0].data.push(item["searchCount"]);
-        });
-        console.log(this.lineDataCollection)
-        // arr.map(item => {
-        //   this.lineDataCollection.labels.push(item[0]);
-        //   this.lineDataCollection.datasets[0].data.push(item[1]);
-        //   counter++;
-        // });
-        // this.$refs.dateChart.scrollLeft += 300;
-        // setTimeout(() => {
-        //   document.querySelector(
-        //     ".dateChart"
-        //   ).scrollLeft -= document.querySelector(".dateChart").scrollWidth;
-        // }, 1000);
-        // console.log(document.querySelector(".dateChart").scrollWidth);
-        // this.pieDataCollection.
-      })
-      .catch(err => {
-        console.log(err);
+        // pieDataCollection.value.datasets[0].backgroundColor.push(
+        //   backgroundColors[counter % 7]
+        // );
+        // pieDataCollection.value.datasets[0].borderColor.push(
+        //   borderColors[counter % 7]
+        // );
+        counter++;
       });
-    // console.log(this.pieDataCollection);
-  },
-  methods: {
-    updateSearchCount() {
-      setTimeout(() => {
-        this.updateTime = Math.random() * 7000;
-        this.totalSearches++;
-        this.updateSearchCount();
-      }, this.updateTime);
-    }
-  }
-};
+      console.log(top30CollegeNames);
+      console.log(top30CollegeSearchCounts);
+
+      totalSearchesOptions.value = {
+        labels: top30CollegeNames,
+      };
+      totalSearchesSeries.value = top30CollegeSearchCounts;
+      console.log(totalSearchesSeries.value);
+      console.log(totalSearchesOptions.value);
+
+      // arr = Object.entries(res.data.searchDates);
+      // set the line chart color to primary
+      // lineDataCollection.datasets[0].backgroundColor.push(backgroundColors[1]);
+      // lineDataCollection.datasets[0].borderColor.push(borderColors[1]);
+      const dailySeachesDates = [];
+      const dailySeachesCounts = [];
+      res.data.searchesArr.map((item, index) => {
+        dailySeachesDates.push(item["date"].slice(5, 10));
+        dailySeachesCounts.push(item["searchCount"]);
+        // lineDataCollection.labels.push(item["date"].slice(5, 10));
+        // lineDataCollection.datasets[0].data.push(item["searchCount"]);
+      });
+      // totalSearchesOptions.value = {
+      //   xaxis: { categories: top30CollegeSearchCounts },
+      // };
+      // totalSearchesSeries.value = [{ name: "count", data: top30CollegeNames }];
+      dailySearchesOptions.value = {
+        xaxis: { categories: dailySeachesDates },
+      };
+      dailySearchesSeries.value = [
+        {
+          name: "searches this day",
+          data: dailySeachesCounts,
+        },
+      ];
+      // console.log(dailySearchesOptions.value);
+      // console.log(dailySearchesSeries.value);
+
+      // console.log(lineDataCollection);
+      // arr.map((item) => {
+      //   lineDataCollection.labels.push(item[0]);
+      //   lineDataCollection.datasets[0].data.push(item[1]);
+      //   counter++;
+      // });
+      // dateChart.scrollLeft += 300;
+      // setTimeout(() => {
+      //   document.querySelector(".dateChart").scrollLeft -=
+      //     document.querySelector(".dateChart").scrollWidth;
+      // }, 1000);
+      // console.log(document.querySelector(".dateChart").scrollWidth);
+      // this.pieDataCollection.
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  // console.log(this.pieDataCollection);
+});
 </script>
 
 <style>
 @media screen and (min-width: 1000px) {
-  .container {
-  }
   .container > div {
     width: 1000px;
   }
