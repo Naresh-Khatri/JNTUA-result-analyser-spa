0d412bd4600435f56b591832f310deec13c8108a
Naresh <naresh.khatri2345@gmail.com>
2022-03-13 23:13:38 +0530

    Remove tailwindcss

--- .postcssrc.js
+++ .postcssrc.js
@@ -3,7 +3,7 @@
 module.exports = {
   plugins: [
     // to edit target browsers: use "browserslist" field in package.json
-    require('tailwindcss'),
     require('autoprefixer')
+    // require('tailwindcss'),
   ]
 }
